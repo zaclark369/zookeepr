9cdf327436211a8cbf239a7d4d5d1af54f680474
Zachary Clark <zacclark369@gmail.com>
2022-01-17 16:59:15 -0800

    changed order of const in server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { animals } = require("./data/animals.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { animals } = require("./data/animals.json");
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
